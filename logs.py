f4e598386e03810c4a95501733be186008202026
Alberto Noguchi <alberto2611@gmail.com>
2024-03-12 18:46:29 -0300

    add: rotating filehandler logs

--- logs.py
+++ logs.py
@@ -2,32 +2,38 @@
 
 import os
 import logging # Root logger
-
+from logging import handlers
 
 # BOILERPLATE
 # TODO: usar função
 # TODO: usar lib (loguru)
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("Beto", log_level) 
-ch = logging.StreamHandler() # Console/terminal/stderr
-ch.setLevel(log_level)
+# ch = logging.StreamHandler() # Console/terminal/stderr
+#ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log", 
+    maxBytes=10**6, 
+    backupCount=10,
+)
+fh.setLevel(log_level)
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
+#ch.setFormatter(fmt)
+# log.addHandler(ch)
+fh.setFormatter(fmt)
+log.addHandler(fh)
 
+"""
 log.debug("Mensagem para o desenvolvedor, qe, sysadmin")
 log.info("Mensagem geral para usuários")
 log.warning("Aviso que não causa erro")
 log.error("Erro que afeta uma unica execução")
 log.critical("Erro geral ex: banco de dados sumiu")
-
-print("-" * 10)
+"""
 
 try:
     1 / 0
 except ZeroDivisionError as e:
     log.error("Deu erro %s", str(e))
-    # stderr
-    
