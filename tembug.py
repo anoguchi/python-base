fd88980fd22ad479c5439feb4f950cfa6bab8637
Alberto Noguchi <alberto2611@gmail.com>
2024-03-20 00:52:38 -0300

    add: debugging

---
+++ tembug.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+def repete_vogal(word):
+    """Retorna a palavra com as vogais repetidas."""
+    final_word = ""
+    # Usamos enumerate para ajudar a sabermos as voltas do loop
+
+    for letter in word:
+        if letter.lower() in "aeiouãõâôêéáíó":
+            final_word = letter * 2
+        else:
+            final_word = letter
+    return final_word
+
+print(repete_vogal("Banana"))
